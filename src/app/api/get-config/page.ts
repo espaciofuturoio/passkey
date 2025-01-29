76029cf6c0110928423d1975d74ed3ca8219c0d0
espaciofuturoio <dev@espaciofuturo.io>
2025-01-29 14:36:01 -0600

    chore: add ping endpoint

---
+++ src/app/api/get-config/page.ts
@@ -0,0 +1,15 @@
+import { type NextRequest, NextResponse } from "next/server";
+export async function GET(req: NextRequest) {
+	try {
+		const input = {
+			origin: req.headers.get("origin") || "",
+		};
+		console.log("Getting config", input);
+		return NextResponse.json(input);
+	} catch (error) {
+		return NextResponse.json(
+			{ error: "Failed to get config" },
+			{ status: 500 },
+		);
+	}
+}
